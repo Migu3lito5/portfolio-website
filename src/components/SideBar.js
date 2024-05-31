12d7cca9e9c9991381c264fc689502e97c228f2e
Miguel A. Francisco <migufran4594@gmail.com>
2024-05-31 14:25:35 -0700

    Improved side bar design with icons and transitions

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,35 +1,42 @@
 "use client"
-import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import React, { useEffect } from 'react';
+import { Link, animateScroll} from 'react-scroll';
 import styles from '@/styles/sidebar.module.css';
+import { HiHome } from "react-icons/hi";
+import { MdOutlineWork } from "react-icons/md";
 
 const SideBar = () => {
-
+  useEffect(() => window.scrollTo({ top: 2}), []);
   return (
     <div className={styles.sidebar}>
       <div className={styles.content}>
         <ul>
           <li>
             <Link
+              className={styles.linkContainer}
               activeClass={styles.active}
               to="intro"
               spy={true}
               smooth={true}
               offset={-200}
               duration={1000}
+              
             >
+              <HiHome className={styles.icon}/>
               Intro
             </Link>
           </li>
           <li>
             <Link
+              className={styles.linkContainer}
               activeClass={styles.active}
               to="projects"
               spy={true}
               smooth={true}
               offset={-110}
               duration={1000}
-            >
+             >
+              <MdOutlineWork className={styles.icon} />
               Projects
             </Link>
           </li>
